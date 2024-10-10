012e887357e28ac6e51f18b322c6c6f6ff9345eb
WYZhang-Git <147374572+WYZhang-Git@users.noreply.github.com>
2024-10-11 00:34:30 +0200

    Consistentie van namen bijgewerkt, toevoeging van shuffle voor horeca, lievelingsattractie toegevoegd, max aantal keer toegevoegd

--- DP5 - startcode v2/Startcode/start_code.py
+++ DP5 - startcode v2/Startcode/start_code.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import json
 from database_wrapper import Database
+import random
 
 def overzicht_attracties():
     # altijd verbinding openen om query's uit te voeren
@@ -39,7 +40,7 @@ list_met_voorzieningen = overzicht_attracties()
 print(list_met_voorzieningen)
 
 print("\n")
-print(f"Dagplanner van: {json_dict['naam']} \n")
+print(f"Dagprogramma van: {json_dict['naam']} \n")
 
 json_bestand.close() # sluit het bestand indien niet meer nodig
 
@@ -49,120 +50,112 @@ json_bestand.close() # sluit het bestand indien niet meer nodig
 # Zorg dat het persoonlijke programma genereert/output naar een .json bestand, dat weer ingelezen kan worden in een webomgeving (zie acceptatieomgeving website folder)
 # Hieronder een begin...
 
-# Initialiseer een lege lijst die de dagplanning zal bevatten
+# Initialiseer een lege lijst die de dagprogramma zal bevatten
 
-dagplanning = []
+dagprogramma = []
 
-# Variabelen om de totale tijd van de dagplanning bij te houden
+# Variabelen om de totale tijd van de dagprogramma bij te houden
 totale_tijd = 0
-verblijfsduur = json_dict['verblijfsduur'] # Verblijfsduur van de bezoeker (in minunten)
+verblijfsduur = json_dict['verblijfsduur']  # Verblijfsduur van de bezoeker (in minuten)
 horeca_moment_interval = 120  # Om de 2 uur wordt een horecagelegenheid toegevoegd 
 laatste_horeca_moment = 0  # Houdt bij wanneer de laatste horecagelegenheid was toegevoegd
 
 # Functie om te checken of een bezoeker aan de eisen voldoet van een voorziening
-def toegankelijkheid_voorziening(voorziening, bezoeker):
+def toegankelijkheid_voorziening(attractie, bezoeker):
     return (
-        (voorziening['attractie_min_leeftijd'] is None or bezoeker['leeftijd'] >= voorziening['attractie_min_leeftijd']) and
-        (voorziening['attractie_min_lengte'] is None or bezoeker['lengte'] >= voorziening['attractie_min_lengte']) and
-        (voorziening['attractie_max_lengte'] is None or bezoeker['lengte'] <= voorziening['attractie_max_lengte']) and  
-        (voorziening['attractie_max_gewicht'] is None or bezoeker['gewicht'] <= voorziening['attractie_max_gewicht'])
+        (attractie['attractie_min_leeftijd'] is None or bezoeker['leeftijd'] >= attractie['attractie_min_leeftijd']) and
+        (attractie['attractie_min_lengte'] is None or bezoeker['lengte'] >= attractie['attractie_min_lengte']) and
+        (attractie['attractie_max_lengte'] is None or bezoeker['lengte'] <= attractie['attractie_max_lengte']) and  
+        (attractie['attractie_max_gewicht'] is None or bezoeker['gewicht'] <= attractie['attractie_max_gewicht'])
     )
 
-def bereken_totale_geschatte_tijd(voorziening):
-    return voorziening['geschatte_wachttijd'] + voorziening['doorlooptijd']
+def bereken_totale_geschatte_tijd(attractie):
+    return attractie['geschatte_wachttijd'] + attractie['doorlooptijd']
 
-def voorkeur_eten_check(voorziening):
+def voorkeur_eten_check(horeca):
     voorkeuren = json_dict['voorkeuren_eten']
+    return horeca['productaanbod'].capitalize() in voorkeuren
+
+# Functie om te controleren of een voorziening al in de dagprogramma staat
+def dagprogramma_controle(dagprogramma, voorziening, max_aantal = 1):
+    aantal = sum(1 for item in dagprogramma if item['naam'] == voorziening['naam'])
     
-    return voorziening['productaanbod'].capitalize() in voorkeuren
+    if voorziening['naam'] in json_dict['lievelings_attracties']:
+        return aantal < 2  # Lievelingsattracties kunnen twee keer voorkomen
+    else:
+        return aantal < max_aantal  # Normale attracties maar één keer
 
 # Functie om horecagelegenheid toe te voegen
-def voeg_horecagelegenheid_toe(dagplanning, totale_tijd, laatste_horeca_moment):
-    for voorziening in list_met_voorzieningen:
-        if voorziening['type'] == 'horeca':
-            if voorkeur_eten_check(voorziening) or not json_dict['voorkeuren_eten']:
-                totale_geschatte_tijd_horeca = bereken_totale_geschatte_tijd(voorziening)
+
+def voeg_horecagelegenheid_toe(dagprogramma, totale_tijd, laatste_horeca_moment):
+    beschikbare_horecagelegenheden = [horeca for horeca in list_met_voorzieningen if horeca['type'] == 'horeca']
+    
+    # Willekeurige horecagelegenheid 
+    if not json_dict['voorkeuren_eten']:
+        random.shuffle(beschikbare_horecagelegenheden)  
+    
+    for horeca in beschikbare_horecagelegenheden:
+            # Controleer of de voorkeur voor eten van toepassing is
+            if voorkeur_eten_check(horeca) or not json_dict['voorkeuren_eten']:
+                totale_geschatte_tijd_horeca = bereken_totale_geschatte_tijd(horeca)
                 if totale_tijd + totale_geschatte_tijd_horeca <= verblijfsduur:
-                    dagplanning.append(voorziening)
+                    dagprogramma.append(horeca)
                     totale_tijd += totale_geschatte_tijd_horeca
                     laatste_horeca_moment = totale_tijd
-                    print(f"Horecagelegenheid toegevoegd: {voorziening['naam']}, totale tijd: {totale_tijd} minuten")  # Voor overzicht bij het testen
-                    break  # Voeg alleen maar één horecagelegenheid toe aan de dagplanning
+                    print(f"Horecagelegenheid toegevoegd: {horeca['naam']}, totale tijd: {totale_tijd} minuten")  # Voor overzicht bij het testen
+                    break  # Voeg alleen maar één horecagelegenheid om de 2 uur
     return totale_tijd, laatste_horeca_moment
-    
-# Doorloop de lijst van voorzieningen en voeg attracties toe die aan de voorkeuren van de bezoeker voldoen
+
+# Voeg eerst een lievelingsattracties toe
+for lievelings_attractie in list_met_voorzieningen:
+    if lievelings_attractie['naam'] in json_dict['lievelings_attracties'] and toegankelijkheid_voorziening(lievelings_attractie, json_dict):
+        totale_geschatte_tijd_attractie = bereken_totale_geschatte_tijd(lievelings_attractie)
+        if totale_tijd + totale_geschatte_tijd_attractie <= verblijfsduur:
+            if dagprogramma_controle(dagprogramma, lievelings_attractie):  
+                dagprogramma.append(lievelings_attractie)
+                totale_tijd += totale_geschatte_tijd_attractie
+                print(f"Lievelingsattractie toegevoegd: {lievelings_attractie['naam']}, totale tijd: {totale_tijd} minuten") # Voor overzicht bij het testen
+
+# Doorloop de lijst van voorzieningen en voeg horecagelegenheden en souvenirwinkel toe
 for voorziening in list_met_voorzieningen:
     # Voeg een souvenirwinkel toe als er 45 minuten of minder over zijn
-    if verblijfsduur - totale_tijd <= 45:
-        for voorziening in list_met_voorzieningen:
-            if voorziening['type'] == 'winkel':
-                dagplanning.append(voorziening)
-                print(f"Souvenirwinkel toegevoegd: {voorziening['naam']}")
-                totale_tijd += bereken_totale_geschatte_tijd(voorziening)
-                break  # Voeg alleen maar één souvenirwinkel toe
-        break  # Stop de loop om geen attracties of horecagelegenheden meer toe te voegen
+    if verblijfsduur - totale_tijd <= 45 and voorziening['type'] == 'winkel':
+        
+        dagprogramma.append(voorziening)
+        print(f"Souvenirwinkel toegevoegd: {voorziening['naam']}") # Voor overzicht bij het testen
+        totale_tijd += bereken_totale_geschatte_tijd(voorziening)
+        break  # Voeg alleen maar één souvenirwinkel toe
 
     # Controleer of het tijd is om een horecagelegenheid toe te voegen
-    if totale_tijd - laatste_horeca_moment >= horeca_moment_interval:
-        totale_tijd, laatste_horeca_moment = voeg_horecagelegenheid_toe(dagplanning, totale_tijd, laatste_horeca_moment)
-    
-    # Voeg voorkeursattracties toe als er genoeg tijd is
-    if voorziening['type'].capitalize() in json_dict['voorkeuren_attractietypes'] and toegankelijkheid_voorziening(voorziening, json_dict):
-        totale_geschatte_tijd_attractie = bereken_totale_geschatte_tijd(voorziening)
+    if verblijfsduur > 120 and totale_tijd - laatste_horeca_moment >= horeca_moment_interval:
+        totale_tijd, laatste_horeca_moment = voeg_horecagelegenheid_toe(dagprogramma, totale_tijd, laatste_horeca_moment)
 
-        # Controleer of er genoeg tijd is
-        if totale_tijd + totale_geschatte_tijd_attractie <= verblijfsduur:  
-            dagplanning.append(voorziening)
-            totale_tijd += totale_geschatte_tijd_attractie
-            print(f"Toegevoegd: {voorziening['naam']}, totale tijd: {totale_tijd} minuten")
-        else:
-            print(f"Niet genoeg tijd voor: {voorziening['naam']}")
+    # Voeg voorkeursattracties toe als er genoeg tijd is, maar alleen als ze nog niet op het dagprogramma staan
+    if voorziening['type'].capitalize() in json_dict['voorkeuren_attractietypes'] and toegankelijkheid_voorziening(voorziening, json_dict) or not json_dict["voorkeuren_attractietypes"]:
+        if dagprogramma_controle(dagprogramma, voorziening):  # Voorkeur attractietypes maar één keer toevoegen
+            totale_geschatte_tijd_attractie = bereken_totale_geschatte_tijd(voorziening)
+            if totale_tijd + totale_geschatte_tijd_attractie <= verblijfsduur:
+                dagprogramma.append(voorziening)
+                totale_tijd += totale_geschatte_tijd_attractie
+                print(f"Toegevoegd: {voorziening['naam']}, totale tijd: {totale_tijd} minuten") # Voor overzicht bij het testen
+            else:
+                print(f"Niet genoeg tijd voor: {voorziening['naam']}") # Voor overzicht bij het testen
 
-# Voeg een horecagelegenheid toe als de bezoeker langer dan 4 uur verblijft
-if verblijfsduur > 240 and totale_tijd - laatste_horeca_moment >= horeca_moment_interval:
-    totale_tijd, laatste_horeca_moment = voeg_horecagelegenheid_toe(dagplanning, totale_tijd, laatste_horeca_moment)
-
-
-
- 
-
-# # Doorloop de lijst van voorzieningen (attracties) en voeg attracties toe die aan de voorkeuren van de klant voldoen
-
-# for voorziening in list_met_voorzieningen:
-
-#     # Controleer of het type van de attractie overeenkomt met de voorkeuren van de klant
-
-#     # Let op: .capitalize() zorgt ervoor dat de vergelijking ongevoelig is voor hoofdletters/kleine letters
-
-#     if voorziening['type'].capitalize() in json_dict['voorkeuren_attractietypes']:
-
-#         # Print de naam en het type van de geselecteerde attractie voor testdoeleinden (debugging)
-
-#         print(f"{voorziening['naam']} - {voorziening['type']}")
-
-#         # Voeg de attractie toe aan de lijst van geselecteerde attracties
-
-#         dagplanning.append(voorziening)
-
- 
-
-# # Genereer het dagprogramma voor de bezoeker
-
-# # Dit programma bevat de naam van de bezoeker en een lijst van geselecteerde attracties op basis van zijn/haar voorkeuren
+# Dit programma bevat de naam van de bezoeker en een lijst van geselecteerde attracties op basis van zijn/haar voorkeuren
 
 dagprogramma = {
     "voorkeuren": {
         "naam": json_dict["naam"],  # Naam van de bezoeker uit JSON  
         "gender": json_dict["gender"],  # Geslacht van de bezoeker uit JSON  
-        "leeftijd": json_dict["leeftijd"], # Leeftijd van de bezoeker uit JSON  
-        "lengte": json_dict["lengte"], # Lengte van de bezoeker uit JSON  
-        "gewicht": json_dict["gewicht"], # Gewicht van de bezoeker uit JSON  
-        "verblijfsduur": json_dict["verblijfsduur"], # Verblijfsduur van de bezoeker uit JSON  
-        "voorkeuren_attractietypes": json_dict["voorkeuren_attractietypes"], # Voorkeuren attractietypes van de bezoeker uit JSON  
-        "lievelings_attracties": json_dict["lievelings_attracties"], # Lieveling attracties van de bezoeker uit JSON  
-        "rekening_houden_met_weer": json_dict["rekening_houden_met_weer"], # Rekening houden met het weer (ja/nee) uit JSON  
+        "leeftijd": json_dict["leeftijd"],  # Leeftijd van de bezoeker uit JSON  
+        "lengte": json_dict["lengte"],  # Lengte van de bezoeker uit JSON  
+        "gewicht": json_dict["gewicht"],  # Gewicht van de bezoeker uit JSON  
+        "verblijfsduur": json_dict["verblijfsduur"],  # Verblijfsduur van de bezoeker uit JSON  
+        "voorkeuren_attractietypes": json_dict["voorkeuren_attractietypes"],  # Voorkeuren attractietypes van de bezoeker uit JSON  
+        "lievelings_attracties": json_dict["lievelings_attracties"],  # Lieveling attracties van de bezoeker uit JSON  
+        "rekening_houden_met_weer": json_dict["rekening_houden_met_weer"],  # Rekening houden met het weer (ja/nee) uit JSON  
     },
-    "voorzieningen": dagplanning  # Voeg de geselecteerde attracties en horeca toe aan het programma
+    "voorzieningen": dagprogramma  # Voeg de geselecteerde attracties en horeca toe aan het programma
 }
 
 # uiteindelijk schrijven we de dictionary weg naar een JSON-bestand
